46f83777dd60e429033e25edfeeca4c2c3da15e0
Myp3ik <mp3ik@inbox.lv>
2017-03-01 01:43:51 +0100

    Diagonal Sudoku solution

--- solution.py
+++ solution.py
@@ -13,8 +13,9 @@ boxes = cross(rows, cols)
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
+diagonal_units = [[rows[int(i) - 1] + i for i in cols], [rows[len(cols) - int(i)] + i for i in cols]]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-unitlist = row_units + column_units + square_units
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
 
